51bdb9f5bf3e91c9aba7da1ec6d6b83851a1b2ef
Vikram Singh <67967129+vikky413@users.noreply.github.com>
2022-11-22 13:00:43 +0530

    Update parsejson.js

--- parsejson.js
+++ parsejson.js
@@ -18,7 +18,7 @@ export const options = {
   duration:duration,
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<600'], // 95% of requests should be below 600ms
+    http_req_duration: ['p(95)<1000'], // 95% of requests should be below 600ms
   },
   
 }
